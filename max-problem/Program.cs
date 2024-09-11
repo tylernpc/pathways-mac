358d72b05c5510b6fd79208bf1d71b7279d0f5c0
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-09-10 21:50:44 -0500

    New Commit

---
+++ max-problem/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Algorithm */
+// Step 1: Get user input while utilizing loop
+// Step 2: Loop the first number (Users five num)
+// Step 3: Math.Max() largest number
+
+// Declare Variables
+int[] userNumbers = new int[5];
+
+// For Loop Logic
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Please Enter A Number Ranging Between 1-100: ");
+    // control veriable change below
+    userNumbers[i] = int.Parse(Console.ReadLine());
+}
