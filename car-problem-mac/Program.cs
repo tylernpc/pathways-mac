c5970b6f1e7e32b1c0a59057e0c30e4325041f02
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-09-12 09:08:11 -0500

    Day 4, Review

--- car-problem-mac/Program.cs
+++ car-problem-mac/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// method testing, and practice. also arrays
+using System;
+using System.Security.Cryptography;
+
+public class methodPractice
+{
+    public static void Main(string[] args)
+    {
+        // declaring standard variables
+        string selectedBrand;
+        
+
+        // creating a basic array, and declaring them
+        string[] germanBrands = {"Porsche", "Mercedes", "Audi", "BMW"};
+        string[] americanBrands = {"Ford", "Chevy", "Tesla", "Dodge",};
+
+        // prompting the user
+        Console.Write("Hello, what car brand are you interested in looking at today: ");
+
+
+    }
+
+    // method that gives the do while loop
+    static void carWhileLoop()
+    {
+        string selectedBrand;
+
+        do
+        {
+            Console.WriteLine("Are you interested in American or German cars? ");
+            selectedBrand = Console.ReadLine().ToUpper();
+
+            if (selectedBrand != "GERMAN" && selectedBrand != "AMERICAN")
+            {
+                Console.WriteLine("Please select either German or American");
+            }
+        } while (selectedBrand != "GERMAN" && selectedBrand != "AMERICAN");
+
+        do
+        {
+            if (selectedBrand == "Porsche")
+            {
+                selectedBrand == "Thing"
+            }
+
+        } while ()
+    }
+}
