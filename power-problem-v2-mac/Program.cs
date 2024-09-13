ae8c406e9b064cb87bc9ca6c77a46ed8c1b9a51d
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-09-12 22:19:58 -0500

    Done for the night

--- power-problem-v2-mac/Program.cs
+++ power-problem-v2-mac/Program.cs
@@ -9,8 +9,13 @@ namespace powerProblemV2
     {
         static void Main(string[] args)
         {
-            // prompts user to input a number
-                // checked if valid (with method)
+            /*
+            loop that rights output in order
+            prompts user for base number
+            prompts user for low exponent
+            prompts user for high exponent
+            do while to check if lower is in order compared to higher
+            */
 
             // declaring variables
             int baseNum, lowExponent, highExponent, loopAmount;
@@ -35,21 +40,28 @@ namespace powerProblemV2
                     lowExponent = intValidation();
                     highExponent = intValidation();
                 }
-            } while (lowExponent > highExponent);
+            } while (lowExponent > highExponent);   // end of do while loop
 
             // lowExponent to highExponent
             for (int i = lowExponent; i <= highExponent; i++)
             {
                 int result = powerMethod(baseNum, i);
                 Console.WriteLine($"{baseNum}^{i} = {result}");
-            }
+            }   // end of for loop
         }
 
         static int intValidation()
         {
+            /*
+            prompts user for a valid integer
+            utilizes a do while loop that checks 
+            */
+
+            // declaring variables
             bool isInt;
             int userExponent;
 
+            // prompts user for valid integer
             Console.Write("Please enter a valid integer: ");
             do
             {
@@ -60,7 +72,7 @@ namespace powerProblemV2
                 {
                     Console.WriteLine("Please Enter a Valid Integer");
                 }
-            } while (!isInt || userExponent < 1);
+            } while (!isInt || userExponent < 1);   // end of while loop
 
             return userExponent;
         }
