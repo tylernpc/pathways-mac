86af99f7cc8da6f6867b6814c33f315705503392
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-09-12 22:04:52 -0500

    v2 Complete

--- power-problem-v2-mac/Program.cs
+++ power-problem-v2-mac/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Diagnostics.Metrics;
+using System.Net.Security;
 
 namespace powerProblemV2
 {
@@ -8,16 +9,48 @@ namespace powerProblemV2
     {
         static void Main(string[] args)
         {
-            int baseNum, lowExponent, highExponent;
+            // prompts user to input a number
+                // checked if valid (with method)
+
+            // declaring variables
+            int baseNum, lowExponent, highExponent, loopAmount;
             
+            // user input for base number
+            Console.Write("Please enter the base number: ");
+            baseNum = intValidation();
+
+            // user input for low exponent
+            Console.Write("Please enter the low exponent: ");
+            lowExponent = intValidation();
+
+            // user input for the high exponent
+            Console.Write("Please enter the high exponent: ");
+            highExponent = intValidation();
+
+            do
+            {
+                if (lowExponent > highExponent)
+                {
+                    Console.WriteLine("Please make sure the low exponent comes before the high");
+                    lowExponent = intValidation();
+                    highExponent = intValidation();
+                }
+            } while (lowExponent > highExponent);
+
+            // lowExponent to highExponent
+            for (int i = lowExponent; i <= highExponent; i++)
+            {
+                int result = powerMethod(baseNum, i);
+                Console.WriteLine($"{baseNum}^{i} = {result}");
+            }
         }
 
-        static int getValidInt()
+        static int intValidation()
         {
             bool isInt;
             int userExponent;
 
-            Console.WriteLine("Please enter a valid integer: ");
+            Console.Write("Please enter a valid integer: ");
             do
             {
                 string input = Console.ReadLine();
@@ -32,10 +65,10 @@ namespace powerProblemV2
             return userExponent;
         }
 
-        static bool powerMethod(int num1, int num2)
+        static int powerMethod(int num, int exponent)
         {
             int result;
-            result = (int)Math.Pow(num1, exponent)
+            result = (int)Math.Pow(num, exponent);
 
             return result;
         }
