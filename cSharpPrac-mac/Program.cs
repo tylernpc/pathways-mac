8f73cd89137ae91d2a5550d346870b0b5002d76f
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-09-13 13:53:39 -0500

    PC REPOS ADDED

---
+++ cSharpPrac-mac/Program.cs
@@ -0,0 +1,107 @@
+﻿/* C# Syntax */
+Console.WriteLine("Hello, World!");
+
+/* C# Comments */
+// Here is a single line comment
+/* Here is a multi
+line comment */
+
+/* C# Variables */
+// int
+int myNum = 50;
+// string
+string myName = "Tyler";
+// basic addition
+int x = 5;
+int y = 10;
+Console.WriteLine(x + y);
+// assigning var to additives
+int a = 5;
+int b = 10;
+int c = a + b;
+Console.WriteLine(c);
+// comma splits
+int d = 5, e = 6, f = 50;
+Console.WriteLine(d + e + f);
+
+/* C# Data Types */
+int num = 9;
+double myDoubleNum = 8.99;
+char myLetter = 'A';
+bool myBoolean = false;
+string myText = "Hello, World!";
+// booleans
+bool yay = true;
+bool nay = false;
+// variables
+string greeting = "Hello";
+Console.WriteLine(greeting);
+// type casting conversion method
+int myInt = 10;
+Console.WriteLine(Convert.ToString(myInt));
+
+/* User Input */
+Console.WriteLine("Enter Username: ");
+string userName = Console.ReadLine();
+Console.WriteLine("Username is: " + userName);
+// Another Example
+Console.WriteLine("Think of a number: ");
+int myNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Your number is: " + myNum);
+
+/* operators */
+Console.WriteLine(10 * 5);
+Console.WriteLine(10 / 5);
+int j = 10;
+j++;
+int z = 10;
+z += 5;
+
+/* math */
+int xx = 5;
+int yy = 10;
+Console.WriteLine(Math.Max(x, y));
+Console.WriteLine(Math.Sqrt(64));
+Console.WriteLine(Math.Round(2.6));
+
+/* interpolation */
+string fName = "John";
+string lName = "Doe";
+string name = $"My full name is: {fName} {lName}";
+
+// single values
+string stringyFName = "Hello";
+Console.WriteLine(stringyFName[0]);
+
+// length of a string
+string txt = "Hello";
+Console.WriteLine(txt.Length);
+
+// Uppercase Something
+Console.WriteLine(txt.ToUpper());
+
+// boolean
+bool isCodingFun = true;
+bool isFishTasty = false;
+Console.WriteLine(isCodingFun);
+Console.WriteLine(isFishTasty);
+
+// ternary operator
+int time = 20;
+string result = (time < 18) ? "Good day." : "Good evening.";
+Console.WriteLine(result);
+
+// while loop
+int i = 1;
+while (i < 6)
+{
+    Console.WriteLine(i);
+    i++;
+}
+
+do
+{
+    Console.WriteLine(i);
+    i++;
+}
+while (i < 6);
---
+++ feet2inches/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+
+public class feet2inches
+{
+    public static void Main(string[] args)
+    {
+        // declare variables
+        double totalFeet = 0;
+
+        do
+        {
+            // Basic Feet to Inches Program | Tyler Le | 9.9.24
+            Console.Write ("How many feet? ");
+            totalFeet = Convert.ToDouble(Console.ReadLine());
+            if (totalFeet < 0)
+            {
+                Console.WriteLine("Please input a real number");
+            }   // end of if
+
+        } 
+        while (totalFeet < 0);   // end of do while
+
+        // basic conversion, and output
+        double totalInches = totalFeet * 12;
+        Console.WriteLine("You have " + totalInches + " inches in total");
+    }
+}
