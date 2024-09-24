4a5583a2b002d5872bd1718b5c50b9e7ebcbefd2
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-09-24 15:36:22 -0500

    Decent Chunk Update

---
+++ music-oop/Program.cs
@@ -0,0 +1,90 @@
+﻿// music program
+using System;
+using System.IO;
+
+namespace musicLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaring variables
+            Music[] music = new Music[10];
+            string fileName = "musicbook.txt";
+            string userArtist;
+            string userSong;
+            int row = 0;
+            int userRating = 0;
+
+            Console.Write("Please enter a(n) artist: ");
+            userArtist = Console.ReadLine();
+
+            Console.Write("Please enter a song by the artist: ");
+            userSong = Console.ReadLine();
+
+            Console.Write("Please enter a rating: ");
+            userRating = Convert.ToInt32(Console.ReadLine());
+
+            // reading and writing portion
+            using (StreamReader sr = File.OpenText(fileName))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null && row < music.Length)
+                {
+                    // split parts based on a delimiter
+                    string[] parts = line.Split('-');
+
+                    // assigning parts to rows and loops through
+                    if (parts.Length == 3)
+                    {
+                        string artist = parts[0];
+                        string song = parts[1];
+                        int rating = Convert.ToInt32(parts[2][0]);
+
+                        music[row] = new Music(artist, song, rating); // stores in an array
+                        row++; // increments through rows
+                    }
+                    Console.WriteLine(line);
+                }
+            }
+
+            // Music userMusic = new Music(userArtist, userSong, userRating);
+        }
+    }
+
+
+
+
+
+
+    class Music
+    {
+        // declared variables
+        public string Artist { get; set; }
+        public string Song { get; set; }
+        public int Rating { get; set; }
+
+        // default constructor when no values are passed
+        public Music()
+        {
+            Artist = "";
+            Song = "";
+            Rating = 0;
+        }
+
+        //constructor when two values are passed
+        public Music(string artist, string song, int rating)
+        {
+            this.Artist = artist;
+            this.Song = song;
+            this.Rating = rating;
+        }
+
+        // override tostring for default display
+        public override string ToString()
+        {
+            return $"Artist: {Artist} | Song: {Song} | Rating: {Rating}";
+        }
+
+    }
+}
