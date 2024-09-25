ec66ed775930a1fb842d0288914adc980132724d
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-09-25 11:46:27 -0500

    iPad Prep Commit

--- music-oop/Classical.cs
+++ music-oop/Classical.cs
@@ -8,16 +8,22 @@ class Classical : Music
     public Classical()
     {
         AlbumYear = "";
+        Artist = "";
+        Album = "";
+        Rating = 0;
     }
 
     // constructor when a value is passed
-    public Classical(string albumYear)
+    public Classical(string albumYear, string artist, string album, int rating)
     {
         this.AlbumYear = albumYear;
+        this.Artist = artist;
+        this.Album = album;
+        this.Rating = rating;
     }
 
     public override string ToString()
     {
-        return $"Album Year: {AlbumYear}";
+        return $"Album Year: {AlbumYear} | Artist: {Artist} | Album: {Album} | Rating: {Rating}";
     }
 }
