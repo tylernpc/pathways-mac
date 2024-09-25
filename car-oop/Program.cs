d97837db57e6f9f0e974e2fc6061777ef4e1036f
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-09-25 11:56:52 -0500

    Updates Sep 25 11:56

---
+++ car-oop/Program.cs
@@ -0,0 +1,39 @@
+﻿// car oop practice
+
+namespace CarProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+
+    class Car
+    {
+        // declaring variables
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Price { get; set; }
+
+        // default constructor
+        public Car()
+        {
+            Brand = "";
+            Model = "";
+            Color = "";
+            Price = 0;
+        }
+
+        // constructor for user
+        public Car(string brand, string model, string color, int price)
+        {
+            this.Brand = brand;
+            this.Model = model;
+            this.Color = color;
+            this.Price = price;
+        }
+    }
+}
