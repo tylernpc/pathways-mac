fcacc806391665abdd87c780e4250235688328e7
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-09-25 15:05:14 -0500

    Override methods added

--- vehicle-oop/Program.cs
+++ vehicle-oop/Program.cs
@@ -9,10 +9,10 @@ namespace CarProgram
             // Vehicle vehicle = new Vehicle("Mercedes", "C300", "Silver", 20000);
             // Console.WriteLine(vehicle);
 
-            Motorcycle motorcycle = new Motorcycle(true, "Ducati", "Panigale V4s", "Red", 30000);
+            Vehicle motorcycle = new Motorcycle(true, "Ducati", "Panigale V4s", "Red", 30000);
             Console.WriteLine(motorcycle);
 
-            Car car = new Car(true, "Mercedes", "C300", "Silver", 20000);
+            Vehicle car = new Car(true, "Mercedes", "C300", "Silver", 20000);
             Console.WriteLine(car);
         }
     }
@@ -34,6 +34,12 @@ namespace CarProgram
             Price = 0;
         }
 
+        // declaring the wheels
+        public virtual int GetWheels()
+        {
+            return 0;
+        }
+
         // constructor for user
         public Vehicle(string brand, string model, string color, int price)
         {
@@ -46,7 +52,7 @@ namespace CarProgram
         // basic override string
         public override string ToString()
         {
-            return $"Brand: {Brand} | Model: {Model} | Color: {Color} | Price: {Price}";
+            return $"Brand: {Brand} | Model: {Model} | Color: {Color} | Price: {Price} | Wheels: {GetWheels()}";
         }
     }
 
@@ -54,6 +60,12 @@ namespace CarProgram
     {
         // declaring variables
         public bool IsMotorcycle { get; set; }
+
+        // declaring a certain amount of wheels for motorcycle (2)
+        public override int GetWheels()
+        {
+            return 2;
+        }
 
         // default constructor
         public Motorcycle():base()
@@ -70,7 +82,7 @@ namespace CarProgram
         // basic override string
         public override string ToString()
         {
-            return $"Is it a Motorcycle: {IsMotorcycle} | Brand: {Brand} | Model: {Model} | Color: {Color} | Price: {Price}";
+            return $"{base.ToString()} | Is it a Motorcycle: {IsMotorcycle}";
         }
     }
 
@@ -78,6 +90,12 @@ namespace CarProgram
     {
         // declaring variables
         public bool IsCar { get; set; }
+
+        // declaring a certain amount of wheels for car (4)
+        public override int GetWheels()
+        {
+            return 4;
+        }
 
         // default constructor
         public Car():base()
@@ -94,7 +112,7 @@ namespace CarProgram
         // basic override string
         public override string ToString()
         {
-            return $"Is it a Car: {IsCar} | Brand: {Brand} | Model: {Model} | Color: {Color} | Price: {Price}";
+            return $"{base.ToString()} | Is it a Car: {IsCar}";
         }
     }
 }
