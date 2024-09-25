add133d175d9b7fb290a2e3a4458a9a31b8e124a
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-09-25 09:04:11 -0500

    Inheritence Class Added

--- music-oop/Program.cs
+++ music-oop/Program.cs
@@ -12,15 +12,15 @@ namespace musicLibrary
             Music[] music = new Music[10];
             string fileName = "musicbook.txt";
             string userArtist;
-            string userSong;
+            string userAlbum;
             int row = 0;
             int userRating = 0;
 
             Console.Write("Please enter a(n) artist: ");
             userArtist = Console.ReadLine();
 
-            Console.Write("Please enter a song by the artist: ");
-            userSong = Console.ReadLine();
+            Console.Write("Please enter a album by the artist: ");
+            userAlbum = Console.ReadLine();
 
             Console.Write("Please enter a rating: ");
             userRating = Convert.ToInt32(Console.ReadLine());
@@ -38,57 +38,76 @@ namespace musicLibrary
                     if (parts.Length == 3)
                     {
                         string artist = parts[0];
-                        string song = parts[1];
+                        string album = parts[1];
                         int rating = Convert.ToInt32(parts[2][0]);
 
-                        music[row] = new Music(artist, song, rating); // stores in an array
+                        music[row] = new Music(artist, album, rating); // stores in an array
                         row++; // increments through rows
                     }
                     // DISPLAYS ALL STUFF THAT'S OPEN Console.WriteLine(line);
                 }
             }
-            // Music userMusic = new Music(userArtist, userSong, userRating);
+            // Music userMusic = new Music(userArtist, userAlbum, userRating);
         }
     }
 
 
 
-
-
-
+    // music class
     class Music
     {
         // declared variables
         public string Artist { get; set; }
-        public string Song { get; set; }
+        public string Album { get; set; }
         public int Rating { get; set; }
 
         // default constructor when no values are passed
         public Music()
         {
             Artist = "";
-            Song = "";
+            Album = "";
             Rating = 0;
         }
 
         //constructor when two values are passed
-        public Music(string artist, string song, int rating)
+        public Music(string artist, string album, int rating)
         {
             this.Artist = artist;
-            this.Song = song;
+            this.Album = album;
             this.Rating = rating;
         }
 
         // override tostring for default display
         public override string ToString()
         {
-            return $"Artist: {Artist} | Song: {Song} | Rating: {Rating}";
+            return $"Artist: {Artist} | Album: {Album} | Rating: {Rating}";
         }
 
     }
 
+
+
+    // classical music class + base
     class Classical : Music
     {
-        public string SongYear { get; set; }
+        // declared variables
+        public string AlbumYear { get; set; }
+
+        // default constructor when no values are passed
+        public Classical()
+        {
+            AlbumYear = "";
+        }
+
+        // constructor when a value is passed
+        public Classical(string albumYear)
+        {
+            this.AlbumYear = albumYear;
+        }
+
+        public override string ToString()
+        {
+            return $"Album Year: {AlbumYear}";
+        }
     }
 }
