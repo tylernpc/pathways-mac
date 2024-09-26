b4e70db89c3772b49f738de997d5dae984f8a470
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-09-26 10:53:28 -0500

    Updates for Hourly and Salary Employees

--- w3-comp/Program.cs
+++ w3-comp/Program.cs
@@ -1,6 +1,8 @@
 ﻿// main program
 using System;
+using System.ComponentModel;
 using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
 
 namespace w3Comp
 {
@@ -8,8 +10,7 @@ namespace w3Comp
     {
         static void Main(string[] args)
         {
-            Employee emp = new Employee("Le", "Tyler", "FullTime");
-            Console.WriteLine(emp);
+            
         }
     }
 
@@ -19,6 +20,7 @@ namespace w3Comp
         public string EmpLName { get; set; }
         public string EmpFName { get; set; }
         public string EmpType { get; set; }
+        public int EmpPay { get; set; }
 
         // default constructor
         public Employee()
@@ -26,20 +28,59 @@ namespace w3Comp
             EmpLName = "";
             EmpFName = "";
             EmpType = "";
+            EmpPay = 0; 
         }
 
         // user constructor
-        public Employee(string empLName, string empFName, string empType)
+        public Employee(string empLName, string empFName, string empType, int empPay)
         {
             this.EmpLName = empLName;
             this.EmpFName = empFName;
             this.EmpType = empType;
+            this.EmpPay = empPay;
+        }
+
+        // override method for bonus
+        public virtual int CalculateBonus()
+        {
+            return 0;
         }
 
         // base override
         public override string ToString()
         {
-            return $"Employee First Name: {EmpFName} / Employee Last Name: {EmpLName} / Employee Type: {EmpType}";
+            return $"Employee First Name: {EmpFName} / Employee Last Name: {EmpLName} / Employee Type: {EmpType} / Salary: {EmpPay}";
         }
+    }
+
+    class Hourly : Employee
+    {
+        // declaring base variables | hourly
+        public bool IsHourly { get; set; }
+
+        // default constructor
+        public Hourly():base()
+        {
+            IsHourly = true; // might have to be false
+        }
+
+        // override method for bonus
+        public override int CalculateBonus()
+        {
+            return 0;
+        }
+    }
+
+    class Salary : Employee
+    {
+        // declaring base variables | salary
+        public bool IsSalary { get; set; }
+
+        // default constructor
+        public Salary():base()
+        {
+            IsSalary = true; // might have to be false
+        }
+
     }
 }
