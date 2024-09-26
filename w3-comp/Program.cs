70ca4d4f6a7b68f3c6450eeb0943671235fdaec0
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-09-26 10:26:52 -0500

    Base Employee Class Set Up

---
+++ w3-comp/Program.cs
@@ -0,0 +1,45 @@
+﻿// main program
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace w3Comp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee("Le", "Tyler", "FullTime");
+            Console.WriteLine(emp);
+        }
+    }
+
+    class Employee
+    {
+        // declaring base variables
+        public string EmpLName { get; set; }
+        public string EmpFName { get; set; }
+        public string EmpType { get; set; }
+
+        // default constructor
+        public Employee()
+        {
+            EmpLName = "";
+            EmpFName = "";
+            EmpType = "";
+        }
+
+        // user constructor
+        public Employee(string empLName, string empFName, string empType)
+        {
+            this.EmpLName = empLName;
+            this.EmpFName = empFName;
+            this.EmpType = empType;
+        }
+
+        // base override
+        public override string ToString()
+        {
+            return $"Employee First Name: {EmpFName} / Employee Last Name: {EmpLName} / Employee Type: {EmpType}";
+        }
+    }
+}
