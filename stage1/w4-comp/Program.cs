f967b24857a728a1953b5f446d58233cf448a0a2
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-04 13:23:47 -0500

    Program Update

--- stage1/w4-comp/Program.cs
+++ stage1/w4-comp/Program.cs
@@ -19,6 +19,22 @@ class Program
                 string accountType = parts[1].Trim();
                 double currentBalance = Convert.ToDouble(parts[2]);
 
+                if (accountType.ToUpper() == "SAVINGS")
+                {
+                    BankAccount savings = new SavingsAccount(0.05, 0, accountID, accountType, currentBalance);
+                    accounts.Add(savings);
+                }
+                else if (accountType.ToUpper() == "CHECKING")
+                {
+                    BankAccount checking = new CheckingAccount(accountID, accountType, currentBalance);
+                    accounts.Add(checking);
+                }
+                else if (accountType.ToUpper() == "CD")
+                {
+                    BankAccount cdAccount = new CDAccount(0.05, 0.10, accountID, accountType, currentBalance);
+                    accounts.Add(cdAccount);
+                }
+
                 // prompt user for type of account, prompts the user for their ID
                 Console.Write("What type of bank account do you have (Savings/Checking/CD): ");
                 string userPromptAccountType = Console.ReadLine().ToUpper();
@@ -26,71 +42,76 @@ class Program
                 string userAccountID = Console.ReadLine();
                 string userPromptOption = "";
 
-                // checks what type of account they have, then checks if their ID matches up
-                if (userPromptAccountType == "SAVINGS")
+                // checks if account exists
+                if (accountID == userAccountID)
                 {
-                    if (accountID == userAccountID)
+                    Console.Write("Would you like to Deposit, Withdraw, or just View: ");
+                    userPromptOption = Console.ReadLine().ToUpper();
+
+                    if (userPromptOption == "DEPOSIT")
                     {
-                        Console.Write("Would you like to Deposit, Withdraw, or just View : ");
-                        userPromptOption = Console.ReadLine().ToUpper();
+                        Console.Write("Enter deposit amount: ");
+                        double depositAmount = Convert.ToDouble(Console.ReadLine());
 
-                        if (userPromptOption == "DEPOSIT")
+                        if (userPromptAccountType == "SAVINGS")
                         {
-                            Console.Write("Enter a deposit amount: ");
-                            double depositAmount = Convert.ToDouble(Console.ReadLine());
+                            BankAccount savings = new SavingsAccount(0.05, 0, accountID, accountType, currentBalance);
+                            savings.Deposit(depositAmount);
+                            Console.WriteLine($"New balance: {savings.CurrentBalance}");
                         }
-                        else if (userPromptOption == "WITHDRAW")
+                        else if (userPromptAccountType == "CHECKING")
                         {
+                            BankAccount checking = new CheckingAccount(accountID, accountType, currentBalance);
+                            checking.Deposit(depositAmount);
+                            Console.WriteLine($"New balance: {checking.CurrentBalance}");
+                        }
+                        else if (userPromptAccountType == "CD")
+                        {
+                            BankAccount cdAccount = new CDAccount(0.05, 0.10, accountID, accountType, currentBalance);
+                            cdAccount.Deposit(depositAmount);
+                            Console.WriteLine($"New balance: {cdAccount.CurrentBalance}");
+                        }
+                    }
+                    else if (userPromptOption == "WITHDRAW")
+                    {
+                        Console.Write("Enter withdrawal amount: ");
+                        double withdrawAmount = Convert.ToDouble(Console.ReadLine());
 
+                        if (userPromptAccountType == "SAVINGS")
+                        {
+                            BankAccount savings = new SavingsAccount(0.05, 0, accountID, accountType, currentBalance);
+                            savings.Withdraw(withdrawAmount);
+                            Console.WriteLine($"New balance: {savings.CurrentBalance}");
                         }
-                        else if (userPromptOption == "VIEW")
+                        else if (userPromptAccountType == "CHECKING")
+                        {
+                            BankAccount checking = new CheckingAccount(accountID, accountType, currentBalance);
+                            checking.Withdraw(withdrawAmount);
+                            Console.WriteLine($"New balance: {checking.CurrentBalance}");
+                        }
+                        else if (userPromptAccountType == "CD")
+                        {
+                            BankAccount cdAccount = new CDAccount(0.05, 0.10, accountID, accountType, currentBalance);
+                            cdAccount.Withdraw(withdrawAmount);
+                            Console.WriteLine($"New balance: {cdAccount.CurrentBalance}");
+                        }
+                    }
+                    else if (userPromptOption == "VIEW")
+                    {
+                        if (userPromptAccountType == "SAVINGS")
                         {
                             BankAccount savings = new SavingsAccount(0.05, 0, accountID, accountType, currentBalance);
                             Console.WriteLine(savings);
                         }
-                    }
-                }
-                if (userPromptAccountType == "CHECKING")
-                {
-                    if (accountID == userAccountID)
-                    {
-                        Console.Write("Would you like to Deposit, Withdraw, or just View : ");
-                        userPromptOption = Console.ReadLine().ToUpper();
-
-                        if (userPromptOption == "DEPOSIT")
-                        {
-
-                        }
-                        else if (userPromptOption == "WITHDRAW")
-                        {
-
-                        }
-                        else if (userPromptOption == "VIEW")
+                        else if (userPromptAccountType == "CHECKING")
                         {
                             BankAccount checking = new CheckingAccount(accountID, accountType, currentBalance);
                             Console.WriteLine(checking);
                         }
-                    }
-                }
-                if (userPromptAccountType == "CD")
-                {
-                    if (accountID == userAccountID)
-                    {
-                        Console.Write("Would you like to Deposit, Withdraw, or just View : ");
-                        userPromptOption = Console.ReadLine().ToUpper();
-
-                        if (userPromptOption == "DEPOSIT")
+                        else if (userPromptAccountType == "CD")
                         {
-
-                        }
-                        else if (userPromptOption == "WITHDRAW")
-                        {
-
-                        }
-                        else if (userPromptOption == "VIEW")
-                        {
-                            BankAccount cdAccounts = new CDAccount(0.05, 0.10, accountID, accountType, currentBalance);
-                            accounts.Add(cdAccounts);
+                            BankAccount cdAccount = new CDAccount(0.05, 0.10, accountID, accountType, currentBalance);
+                            Console.WriteLine(cdAccount);
                         }
                     }
                 }
