674fadffa077c7e545ac5e35c91eb01f9ce96438
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-08 10:00:52 -0500

    NonProfit Updates

--- stage1/w5-comp/ExecutiveAccount.cs
+++ stage1/w5-comp/ExecutiveAccount.cs
@@ -1,4 +1,16 @@
 public class ExecutiveAccount : Membership, ISpecialOffer
 {
-    
+    public override double CashbackRewards()
+    {
+        double executiveCashBack = 0;
+        if (TotalAmountOfSpend < 1000)
+        {
+            executiveCashBack = TotalAmountOfSpend * 0.05; // 5% Cashback hard coded
+        }
+        if (TotalAmountOfSpend > 1000)
+        {
+            executiveCashBack = TotalAmountOfSpend * 0.07; // 7% Cashback hard coded
+        }
+        return executiveCashBack;
+    }
 }
--- stage1/w5-comp/NonProfitAccount.cs
+++ stage1/w5-comp/NonProfitAccount.cs
@@ -1,4 +1,41 @@
 public class NonProfitAccount : Membership
 {
-    
+    public string TypeOfNonProfitMembership { get; set; }
+
+    public NonProfitAccount() : base ()
+    {
+        TypeOfNonProfitMembership = "";
+    }
+
+    public NonProfitAccount(
+        string typeOfNonProfitMembership, 
+        string membershipID, 
+        string emailAddress, 
+        string typeOfMembership, 
+        int annualFee, 
+        double totalAmountOfSpend) 
+        : base (
+            membershipID, 
+            emailAddress, 
+            typeOfMembership, 
+            annualFee, 
+            totalAmountOfSpend)
+    {
+        TypeOfNonProfitMembership = typeOfNonProfitMembership;
+    }
+
+    public override double CashbackRewards()
+    {
+        
+        double executiveCashBack = 0;
+        if (TotalAmountOfSpend < 1000)
+        {
+            executiveCashBack = TotalAmountOfSpend * 0.05; // 5% Cashback hard coded
+        }
+        if (TotalAmountOfSpend > 1000)
+        {
+            executiveCashBack = TotalAmountOfSpend * 0.07; // 7% Cashback hard coded
+        }
+        return executiveCashBack;
+    }
 }
