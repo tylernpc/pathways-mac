aea4edad4305b1391ddf6dff34a2e3c8ae8c6f42
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-11 10:04:02 -0500

    Customer Table Syntax In Place, Not ran yet

---
+++ unstaged/W5-Postgres/Stage One Comp.session.sql
@@ -0,0 +1,8 @@
+CREATE TABLE customer (
+    account_id CHAR(5),
+    email_address VARCHAR(50),
+    account_type VARCHAR(50),
+    annual_fee INT,
+    total_amount_spent INT,
+    type_of_nonprofit VARCHAR(50)
+);
