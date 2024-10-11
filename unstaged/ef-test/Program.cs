1194d1471818430a313eef7fcfbccf163662f2bd
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-11 11:21:58 -0500

    Updates to the main program

--- unstaged/ef-test/Program.cs
+++ unstaged/ef-test/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+namespace EFTEST;
+
+public class AppDbContext : DbContext
+{
+    public DbSet <YourEntity> YourEntities { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=your_host;Database=your_db;Username=tyler;Password=password");
+    }
+}
+
+public class YourEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public YourEntity()
+    {
+        Id = 0;
+        Name = "";
+    }
+
+    public YourEntity(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
