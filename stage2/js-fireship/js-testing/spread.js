c8f5877b2c78654c6865d3e9074258e7ca952cf8
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-14 09:55:56 -0500

    New doc structure

---
+++ stage2/js-fireship/js-testing/spread.js
@@ -0,0 +1,13 @@
+// this is for the spread operator
+
+// combine two objects or arrays together
+const obj1 = {
+    first: 'a',
+    second: 'b',
+    third: 'c',
+}
+
+const obj2 = {
+    third: 'd',
+    fourth: 'e',
+}
