2c953035ff7cc323950cc5a43cf6b2c87892c03e
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-14 10:02:14 -0500

    Spread Operator

--- stage2/js-fireship/js-testing/spread.js
+++ stage2/js-fireship/js-testing/spread.js
@@ -11,3 +11,22 @@ const obj2 = {
     third: 'd',
     fourth: 'e',
 }
+
+const full = Object.assign({}, obj1, obj2);
+const newFull = {...obj1, ...obj2}; // spread syntax
+// in this case obj2 would take priority and third would be d
+
+
+// also spread syntax
+const obj3 = {
+    ten: 'a',
+    eleven: 'b',
+    twelve: 'c',
+}
+
+const obj4 = {
+    ...obj3,
+    twelve: 'n',
+    thirteen: 'd',
+}
+// NOTE: depending on where you put the spread syntax will overwrite certain things. in this case c from obj3 is overwritten
