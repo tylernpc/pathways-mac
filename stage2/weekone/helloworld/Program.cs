086b43c78d9d1464a37b9456bf7bccf236866f24
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-14 16:07:56 -0500

    Program not working, not initializing object correctly

---
+++ stage2/weekone/helloworld/Program.cs
@@ -0,0 +1,43 @@
+namespace helloworld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter your first name: ");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("Please enter your last name: ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine("Please enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+
+        UserName(firstName, lastName, age);
+    }
+}
+
+public class UserName
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+
+    public UserName()
+    {
+        FirstName = "";
+        LastName = "";
+        Age = 0;
+    }
+
+    public UserName(string firstName, string lastName, int age)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+    }
+
+    public override string ToString()
+    {
+        return $"First Name: {FirstName}, Last Name: {LastName}, and Age: {Age}";
+    }
+}
