49b6e029a38697aa52b501a40562dce09da73767
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-14 09:49:20 -0500

    Updates to app.js

--- stage2/js-fireship/js-testing/app.js
+++ stage2/js-fireship/js-testing/app.js
@@ -1,1 +1,22 @@
-console.log("hello, world");
+const dog = {
+    name: 'fido',
+    age: 4,
+    bark () {
+        console.log('woof');
+    }
+}
+
+// dot notation
+// const name = dog.name;
+// const age = dog.age;
+// console.log(name, age);
+
+// destructuring
+const { name, age } = dog;
+
+// another destructuring example / both do the same thing, but line 20 is shorthand of 19 where the placement matters
+const arr = ['foo', 'bar', 'baz'];
+// const [,, c] = arr;
+const c = arr[2];
+
+dog.bark();
