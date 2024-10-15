b9d16f5fc088f8eafad061b80b52aa63a7df71af
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-15 10:26:49 -0500

    Updates To JS

--- stage2/weekone/usersurvey/app.js
+++ stage2/weekone/usersurvey/app.js
@@ -1,1 +1,8 @@
-console.log("hello, world");
+let userFirstName = document.getElementById("fName").value;
+let userLastName = document.getElementById("lName").value;
+
+document.getElementById("submit-btn").addEventListener("click", displayVehicle);
+
+function displayVehicle() {
+  alert(`Hello, ${userFirstName} ${userLastName}`);
+}
