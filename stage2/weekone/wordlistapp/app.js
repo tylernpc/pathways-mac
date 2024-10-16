9d5e61b5a6002da514e96c70474bd227f619eee6
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-16 13:02:40 -0500

    Update

--- stage2/weekone/wordlistapp/app.js
+++ stage2/weekone/wordlistapp/app.js
@@ -1,7 +1,43 @@
 document.getElementById("submit-btn").addEventListener("click", addToList);
 
 function addToList() {
-  console.log("hello, world");
+  const userInput = document.getElementById("UserInputWord").value.trim();
+  const userSelection = document.getElementById("UserSelection").value;
+
+  validateUserInput(userInput);
+  validateUserSelection(userSelection);
+
+  listLogic(userInput, userSelection);
 }
 
-const userInput = document.getElementById("UserInputWord").value;
+function listLogic(userInput, userSelection) {
+  let tableRef;
+
+  if (userSelection == "1") {
+    tableRef = document.getElementById("myList1");
+  } else if (userSelection == "2") {
+    tableRef = document.getElementById("myList2");
+  }
+
+  const newRow = tableRef.inserRow();
+  const newCell = newRow.insertCell(0);
+  newCell.textContent = userInput;
+
+  document.getElementById("UserInputWord").value = "";
+  document.getElementById("UserSelection").value = "";
+}
+
+function validateUserInput(userInput) {
+  if (userInput == "") {
+    wordExist = false;
+  }
+  return true;
+}
+
+function validateUserSelection(userSelection) {
+  if (userSelection == "") {
+    alert("Please select the first or second list.");
+    return false;
+  }
+  return true;
+}
