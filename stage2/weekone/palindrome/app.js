c5dd38e13e11ac3b6d02f494fc25f10cc26822af
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-17 11:58:09 -0500

    Updates for the Palindrome

--- stage2/weekone/palindrome/app.js
+++ stage2/weekone/palindrome/app.js
@@ -1,60 +1,73 @@
-document.getElementById("userPromptForm").addEventListener("submit", function(event) {
+document
+  .getElementById("userPromptForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
-    addToList(); 
+    addToList();
   });
-  
-  function addToList() {
-    const userInput = document.getElementById("UserInputWord").value.trim();
-    const userSelection = document.getElementById("UserSelection").value;
-  
-    if (validateUserInput(userInput) && validateUserSelection(userSelection)) {
-      listLogic(userInput, userSelection);
-    }  
+
+function addToList() {
+  const userInput = document.getElementById("UserInputWord").value.trim();
+  const userSelection = document.getElementById("UserSelection").value;
+
+  if (validateUserInput(userInput) && validateUserSelection(userSelection)) {
+    listLogic(userInput, userSelection);
   }
-  
-  function listLogic(userInput, userSelection) {
-    let tableRef;
-  
-    if (userSelection == "1") {
-      tableRef = document.getElementById("myList1");
-    } else if (userSelection == "2") {
-      tableRef = document.getElementById("myList2");
-    }
-  
-    const newRow = tableRef.insertRow();
-    const newCell = newRow.insertCell(0);
-    newCell.textContent = userInput;
-  
-    document.getElementById("UserInputWord").value = "";
-    document.getElementById("UserSelection").value = "";
+}
+
+function firstAlgorithm(userInput) {
+  userInput = str.toLowerCase().replace(re, "");
+  let palCheck = userInput.split("").reverse().join("");
+  if (palCheck == userInput) return true;
+  else return false;
+}
+
+function secondAlgorithm(userInput) {
+  let palCheck = user;
+}
+
+function listLogic(userInput, userSelection) {
+  let tableRef;
+
+  if (userSelection == "1") {
+    tableRef = document.getElementById("myList1");
+  } else if (userSelection == "2") {
+    tableRef = document.getElementById("myList2");
   }
-  
-  // validation
-  function validateUserInput(userInput) {
-    if (userInput == "") { 
-      return false;
-    }
-    return true;
+
+  const newRow = tableRef.insertRow();
+  const newCell = newRow.insertCell(0);
+  newCell.textContent = userInput;
+
+  document.getElementById("UserInputWord").value = "";
+  document.getElementById("UserSelection").value = "";
+}
+
+// validation
+function validateUserInput(userInput) {
+  if (userInput == "") {
+    return false;
   }
-  
-  function validateUserSelection(userSelection) {
-    if (userSelection == "") {
-      alert("Please select the first or second list.");
-      return false;
-    }
-    return true;
+  return true;
+}
+
+function validateUserSelection(userSelection) {
+  if (userSelection == "") {
+    alert("Please select the first or second list.");
+    return false;
   }
-  
-  // clear logic
-  document.getElementById("clear-btn-1").addEventListener("click", function() {
-    clearList("myList1");
-  });
-  
-  document.getElementById("clear-btn-2").addEventListener("click", function() {
-    clearList("myList2");
-  });
-  
-  function clearList(listId) {
-    const tableRef = document.getElementById(listId);
-    tableRef.innerHTML = ""; 
-  }
+  return true;
+}
+
+// clear logic
+document.getElementById("clear-btn-1").addEventListener("click", function () {
+  clearList("myList1");
+});
+
+document.getElementById("clear-btn-2").addEventListener("click", function () {
+  clearList("myList2");
+});
+
+function clearList(listId) {
+  const tableRef = document.getElementById(listId);
+  tableRef.innerHTML = "";
+}
