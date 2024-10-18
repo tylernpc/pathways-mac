f1b0e6ab412fdf6ba84f368032fd17b043cbceea
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-18 14:37:23 -0500

    Restarting HTML and JS with no CSS, and reupdating this version

--- stage2/weekone/comp/app.js
+++ stage2/weekone/comp/app.js
@@ -5,24 +5,21 @@ const myElement = document
   });
 
 function addItem() {
-  const newItem = document.createElement("div");
-  newItem.setAttribute("contenteditable", "true");
+  const newItem = document.createElement('div');
+  const trashBtn = document.createElement("button");
+  newItem.setAttribute('contenteditable', 'true');
+  trashBtn.innerHTML = '<i class="fas fa-plus-circle fa-lg"></i>';
 
-  const newImg = document.createElement('img');
-  newImg.src = 'images/Trash_Full.png';
-  document.getElementById('body')?.appendChild(newImg);
-
-  const node = document.createTextNode("type here!");
+  const node = document.createTextNode('type here!');
   newItem.appendChild(node);
 
-  const table = document.getElementById("find-me");
+  const table = document.getElementById('find-me');
   table?.appendChild(newItem);
-}
 
-function selectItem() {
-  console.log('hola world');
+  trashBtn.classList.add('trash-btn');
+  table.appendChild(trashBtn);
 }
 
 function deleteItem() {
-  console.log('adios world');
+  document.getElementById('trash-btn');
 }
