c4fecfe60e8f9e08a3102ccdc3f748d934341bd5
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-17 20:39:29 -0500

    updates to the program

--- stage2/weekone/compchallenge/dist/app.js
+++ stage2/weekone/compchallenge/dist/app.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('hello, world');
+var _a;
+const myElement = (_a = document.getElementById('add-btn')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+    addItem();
+});
+function addItem() {
+    const newItem = document.createElement('div');
+    newItem.setAttribute('contenteditable', 'true');
+    const node = document.createTextNode('type here!');
+    newItem.appendChild(node);
+    const table = document.getElementById('find-me');
+    table === null || table === void 0 ? void 0 : table.appendChild(newItem);
+}
--- stage2/weekone/compchallenge/src/app.ts
+++ stage2/weekone/compchallenge/src/app.ts
@@ -1,1 +1,14 @@
-document.getElementById('')
+const myElement = document.getElementById('add-btn')?.addEventListener('click', () => {
+    addItem();
+});
+
+function addItem() {
+    const newItem = document.createElement('div');
+    newItem.setAttribute('contenteditable', 'true');
+
+    const node = document.createTextNode('type here!');
+    newItem.appendChild(node);
+
+    const table = document.getElementById('find-me');
+    table?.appendChild(newItem);
+}
