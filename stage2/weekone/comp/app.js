33170e0de80f7430a311d266f4239539f0d17ee6
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-19 17:17:09 -0500

    Good Updates to HTML and CSS good updates allaround

--- stage2/weekone/comp/app.js
+++ stage2/weekone/comp/app.js
@@ -1,36 +1,51 @@
-const userInput = document.querySelector('.user-input');
-const addButton = document.querySelector('.add-btn');
-const todoList = document.querySelector('.todo-list');
+const userInput = document.querySelector(".user-input");
+const addButton = document.querySelector(".add-btn");
+const todoList = document.querySelector(".todo-list");
 
-addButton.addEventListener('click', function(event) {
-    event.preventDefault();
+userInput.addEventListener("input", function () {
+  if (userInput.value.length > 55) {
+    userInput.value = userInput.value.slice(0, 55);
+  }
+});
 
-    if (userInput.value.trim() === ""){
-        alert('Please input a value');
-        return;
+addButton.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  if (userInput.value.trim() === "") {
+    alert("Please input a value");
+    return;
+  }
+
+  // creating top layer of todo, li subitem will go inside
+  const topItem = document.createElement("div");
+  topItem.classList.add("single-item");
+
+  // creating the list item
+  const subItem = document.createElement("li");
+  subItem.innerText = userInput.value.trim();
+  subItem.classList.add("todo-item");
+  topItem.appendChild(subItem);
+  subItem.setAttribute("contenteditable", "true");
+
+  subItem.addEventListener("input", function () {
+    if (subItem.innerText.length > 55) {
+      subItem.innerText = subItem.innerText.slice(0, 55);
     }
+  });
 
-    // creating top layer of todo, li subitem will go inside
-    const topItem = document.createElement('div');
-    topItem.classList.add('todo');
+  // creating a trash button
+  const trashButton = document.createElement("button");
+  trashButton.classList.add("trash-btn");
+  trashButton.innerText = "Delete";
+  topItem.appendChild(trashButton);
+  todoList.appendChild(topItem);
 
-    // creating the list item
-    const subItem = document.createElement('li');
-    subItem.innerText = userInput.value.trim();
-    subItem.classList.add('todo-item');
-    topItem.appendChild(subItem);
-    subItem.setAttribute("contenteditable", "true");
+  // creating a complete button
+  const completeButton = document.createElement("button");
 
-    // creating a trash button
-    const trashButton = document.createElement('button');
-    trashButton.classList.add('trash-btn');
-    trashButton.innerText = 'Delete';
-    topItem.appendChild(trashButton);
-    todoList.appendChild(topItem);
+  trashButton.addEventListener("click", function () {
+    topItem.remove();
+  });
 
-    trashButton.addEventListener('click', function() {
-        topItem.remove();
-    });
-
-    userInput.value = '';
+  userInput.value = "";
 });
