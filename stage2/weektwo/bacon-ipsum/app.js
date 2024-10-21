9470af5b639a910b5ccad214e4b4e25ac775c41c
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-21 17:21:30 -0500

    Updates

---
+++ stage2/weektwo/bacon-ipsum/app.js
@@ -0,0 +1,36 @@
+async function getBaconIpsum() {
+  let typeOfMeat = getTypeOfMeat();
+  let numOfParas = getNumOfParas();
+  let startWithLorem = getStartWithLorem();
+
+  let apiString;
+  if (startWithLorem === true) {
+    apiString = `https://baconipsum.com/api/?type=${typeOfMeat}&paras=${numOfParas}&start-with-lorem=1`;
+  } else {
+    apiString = `https://baconipsum.com/api/?type=${typeOfMeat}&paras=${numOfParas}`;
+  }
+
+  let response = await fetch(apiString);
+  let jsonData = await response.json();
+
+  document.getElementById("output-json").innerHTML = JSON.stringify(jsonData);
+  return true;
+}
+
+function getTypeOfMeat() {
+  let typeOfMeat = document.getElementById("typeOfMeat").value;
+  if (typeOfMeat === "All") {
+    return "meat";
+  } else if (typeOfMeat === "Filler") {
+    return "meat-and-filler";
+  }
+  return "meat";
+}
+
+function getNumOfParas() {
+  return parseInt(document.getElementById("numOfParas").value);
+}
+
+function getStartWithLorem() {
+  return document.getElementById('startLorem').value === "Yes";
+}
