abc318abb7593eb06bdd7a4cc053624240dda66c
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-24 12:35:18 -0500

    Updates

---
+++ stage2/weektwo/ng-tut/first-app/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HousingLocation } from "../housing-location";
+
+@Component({
+  selector: "app-housing-location",
+  standalone: true,
+  imports: [],
+  template: `
+    <section class="listing">
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+    </section>
+  `,
+  styleUrls: ["./housing-location.component.css"],
+})
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+}
