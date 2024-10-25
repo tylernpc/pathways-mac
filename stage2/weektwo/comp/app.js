284c7786d2c2b25f4619a07c5e3313fb84f9c5bc
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-25 12:42:35 -0500

    Updates To the Whole program, late push

--- stage2/weektwo/comp/app.js
+++ stage2/weektwo/comp/app.js
@@ -1,12 +1,7 @@
-// let response;
-// let jsonData;
-// let apiString;
-
-async function fetchMarketSentiment() {
-  apiString = "https://tradestie.com/api/v1/apps/reddit?date=2024-10-24";
-  response = await fetch(apiString);
-  jsonData = await response.json();  
-}
+// declared var
+let response;
+let jsonData;
+let apiString;
 
 function getUserDate() {
   const dateString = document.getElementById("userDateRange").value;
@@ -14,7 +9,6 @@ function getUserDate() {
 
   const startDate = new Date(startDateStr);
   const endDate = new Date(endDateStr);
-
 
   const userBeginYear = startDate.getFullYear();
   const userBeginMonth = startDate.getMonth() + 1;
@@ -26,9 +20,22 @@ function getUserDate() {
 
   // logical issue with this being a first of the month thing, for this example I am not taking that into account
   const weekCheck = userEndDay - userBeginDay;
-  if (weekCheck >= 5) {
+  if (weekCheck >= 5 || weekCheck < 4) {
     alert("Please only select a date range between 5 days");
   } else {
+    let apiBeginString = `${userBeginYear}-${userBeginMonth}-${userBeginDay}`;
+    
+    for (let i = userBeginDay + 1; i < userEndDay; i++) {
+      let apiMiddleDay = i;
+      console.log(`${userEndYear}-${userEndMonth}-${apiMiddleDay}`); // this is the api key
+    }
+    let apiEndString = `${userEndYear}-${userEndMonth}-${userEndDay}`;
     
   }
 }
+
+// async function fetchMarketSentiment() {
+//   apiString = `https://tradestie.com/api/v1/apps/reddit?date=${userBeginYear}-10-24`;
+//   response = await fetch(apiString);
+//   jsonData = await response.json();
+// }
