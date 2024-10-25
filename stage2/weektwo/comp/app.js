857683c717c440489e9fcbe8f1a77d30c579397c
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-25 10:48:25 -0500

    Good updates to the program overall

---
+++ stage2/weektwo/comp/app.js
@@ -0,0 +1,34 @@
+// let response;
+// let jsonData;
+// let apiString;
+
+async function fetchMarketSentiment() {
+  apiString = "https://tradestie.com/api/v1/apps/reddit?date=2024-10-24";
+  response = await fetch(apiString);
+  jsonData = await response.json();  
+}
+
+function getUserDate() {
+  const dateString = document.getElementById("userDateRange").value;
+  const [startDateStr, endDateStr] = dateString.split(" - ");
+
+  const startDate = new Date(startDateStr);
+  const endDate = new Date(endDateStr);
+
+
+  const userBeginYear = startDate.getFullYear();
+  const userBeginMonth = startDate.getMonth() + 1;
+  const userBeginDay = startDate.getDate();
+
+  const userEndYear = endDate.getFullYear();
+  const userEndMonth = endDate.getMonth() + 1;
+  const userEndDay = endDate.getDate();
+
+  // logical issue with this being a first of the month thing, for this example I am not taking that into account
+  const weekCheck = userEndDay - userBeginDay;
+  if (weekCheck >= 5) {
+    alert("Please only select a date range between 5 days");
+  } else {
+    
+  }
+}
