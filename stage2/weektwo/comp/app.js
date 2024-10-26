14837be56184c9d7424215b0fe68bbe3baf02675
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-26 17:15:15 -0500

    Making dashboard.js the primary js

--- stage2/weektwo/comp/app.js
+++ stage2/weektwo/comp/app.js
@@ -1,52 +1,47 @@
-// declared var
-let response;
-let jsonData;
-let apiString;
+// let response;
+// let jsonData;
 
-function mainLogic() {
-  const dateString = document.getElementById("userDateRange").value;
-  const [startDateStr, endDateStr] = dateString.split(" - ");
+// async function mainLogic() {
+//   const dateString = document.getElementById("userDateRange").value;
+//   const [startDateStr, endDateStr] = dateString.split(" - ");
 
-  const startDate = new Date(startDateStr);
-  const endDate = new Date(endDateStr);
+//   const startDate = new Date(startDateStr);
+//   const endDate = new Date(endDateStr);
 
-  const startDateYear = startDate.getFullYear();
-  const startDateMonth = startDate.getMonth() + 1;
-  const startDateDay = startDate.getDate();
-  const endDateDay = endDate.getDate();
+//   const startDateYear = startDate.getFullYear();
+//   const startDateMonth = startDate.getMonth() + 1;
+//   const startDateDay = startDate.getDate();
+//   const endDateDay = endDate.getDate();
 
-  // logical issue with this being a first of the month thing, for this example I am not taking that into account
-  const weekCheck = endDateDay - startDateDay;
-  if (weekCheck >= 5 || weekCheck < 4) {
-    alert("Please only select a date range between 5 days");
-  } else {
-    async function retrieveJson() {
-      for (let i = startDateDay + 1; i < endDateDay; i++) {
-        let apiMiddleString = `${startDateYear}-${startDateMonth}-${i}`;
-        response = await fetch(apiMiddleString);
-        jsonData = await response.json();
-        return jsonData;
-      }
-    }
+//   const weekCheck = endDateDay - startDateDay;
+//   if (weekCheck >= 5 || weekCheck < 4) {
+//     alert("Please only select a date range between 5 days");
+//   } else {
+//     async function retrieveJson(dateString) {
+//       response = await fetch(dateString);
+//       return await response.json();
+//     }
 
-    const informationTableBody = document.getElementById("information");
-    // 1st step | choose data
-    let ticker = retrieveJson().item.ticker;
-    let sentimentScore = retrieveJson().item.sentiment_score;
-    let sentiment = retrieveJson().item.sentiment;
-  
-    // 2nd step | create data
-    const rows = [ [ticker, sentimentScore, sentiment] ]
+//     const informationTableBody = document.getElementById("information");
 
-    // 3rd step | loop through data
-    rows.forEach((row) => {
-      const tr = document.createElement("tr");
-      row.forEach((cellText) => {
-        const td = document.createElement("td");
-        td.textContent = cellText;
-        tr.appendChild(td);
-      });
-      informationTableBody.appendChild(tr);
-    });
-  }
-}
+//     for (let i = startDateDay + 1; i < endDateDay; i++) {
+//       let apiMainString = `${startDateYear}-${startDateMonth}-${i}`;
+//       jsonData = await retrieveJson(apiMainString);
+
+//       const ticker = jsonData.ticker;
+//       const sentimentScore = jsonData.sentiment_score;
+//       const sentiment = jsonData.sentiment;
+
+//       const row = [ticker, sentimentScore, sentiment];
+
+//       const tr = document.createElement("tr");
+//       row.forEach((cellText) => {
+//         const td = document.createElement("td");
+//         td.textContent = cellText;
+//         tr.appendChild(td);
+//       });
+//       informationTableBody.appendChild(tr);
+//     }
+//   }
+// }
+
--- stage2/weektwo/comp/dashboard.js
+++ stage2/weektwo/comp/dashboard.js
@@ -1,11 +1,61 @@
-/* globals Chart:false */
+// import { mainLogic } from "./app";
 
 (() => {
   "use strict";
 
-  // Graphs
+
+  // api logic
+  let response;
+  let jsonData;
+
+  async function mainLogic() {
+    const dateString = document.getElementById("userDateRange").value;
+    const [startDateStr, endDateStr] = dateString.split(" - ");
+
+    const startDate = new Date(startDateStr);
+    const endDate = new Date(endDateStr);
+
+    const startDateYear = startDate.getFullYear();
+    const startDateMonth = startDate.getMonth() + 1;
+    const startDateDay = startDate.getDate();
+    const endDateDay = endDate.getDate();
+
+    const weekCheck = endDateDay - startDateDay;
+    if (weekCheck >= 5 || weekCheck < 4) {
+      alert("Please only select a date range between 5 days");
+    } else {
+      async function retrieveJson(dateString) {
+        response = await fetch(dateString);
+        return await response.json();
+      }
+
+      const informationTableBody = document.getElementById("information");
+
+      for (let i = startDateDay + 1; i < endDateDay; i++) {
+        let apiMainString = `${startDateYear}-${startDateMonth}-${i}`;
+        jsonData = await retrieveJson(apiMainString);
+
+        const ticker = jsonData.ticker;
+        const sentimentScore = jsonData.sentiment_score;
+        const sentiment = jsonData.sentiment;
+
+        const row = [ticker, sentimentScore, sentiment];
+
+        const tr = document.createElement("tr");
+        row.forEach((cellText) => {
+          const td = document.createElement("td");
+          td.textContent = cellText;
+          tr.appendChild(td);
+        });
+        informationTableBody.appendChild(tr);
+      }
+    }
+  }
+  // api logic
+
+  
   const ctx = document.getElementById("myChart");
-  // eslint-disable-next-line no-unused-vars
+
   const myChart = new Chart(ctx, {
     type: "line",
     data: {
@@ -33,4 +83,3 @@
     },
   });
 })();
-
