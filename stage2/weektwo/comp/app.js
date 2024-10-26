b8ef36bd54856b4681ceaf1cbdd6794471bf9d2e
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-26 16:23:29 -0500

    Restructuring of the app.js file

--- stage2/weektwo/comp/app.js
+++ stage2/weektwo/comp/app.js
@@ -8,19 +8,21 @@ function mainLogic() {
   const [startDateStr, endDateStr] = dateString.split(" - ");
 
   const startDate = new Date(startDateStr);
+  const endDate = new Date(endDateStr);
 
-  const dateYear = startDate.getFullYear();
-  const dateMonth = startDate.getMonth() + 1;
-  const dateDay = startDate.getDate();
+  const startDateYear = startDate.getFullYear();
+  const startDateMonth = startDate.getMonth() + 1;
+  const startDateDay = startDate.getDate();
+  const endDateDay = endDate.getDate();
 
   // logical issue with this being a first of the month thing, for this example I am not taking that into account
-  const weekCheck = userEndDay - userBeginDay;
+  const weekCheck = endDateDay - startDateDay;
   if (weekCheck >= 5 || weekCheck < 4) {
     alert("Please only select a date range between 5 days");
   } else {
     async function retrieveJson() {
-      for (let i = userBeginDay + 1; i < userEndDay; i++) {
-        let apiMiddleString = `${userEndYear}-${userEndMonth}-${i}`;
+      for (let i = startDateDay + 1; i < endDateDay; i++) {
+        let apiMiddleString = `${startDateYear}-${startDateMonth}-${i}`;
         response = await fetch(apiMiddleString);
         jsonData = await response.json();
         return jsonData;
@@ -29,14 +31,12 @@ function mainLogic() {
 
     const informationTableBody = document.getElementById("information");
     // 1st step | choose data
-    let 
+    let ticker = retrieveJson().item.ticker;
+    let sentimentScore = retrieveJson().item.sentiment_score;
+    let sentiment = retrieveJson().item.sentiment;
   
-
-
     // 2nd step | create data
     const rows = [ [ticker, sentimentScore, sentiment] ]
-
-
 
     // 3rd step | loop through data
     rows.forEach((row) => {
