126f31cb7b0345f64dc003d82d29f049dbdaf243
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-11-01 14:00:14 -0500

    Good progress with notification classes

--- stage2/weekthree/Comp/4) Notifications/SmsNotification.cs
+++ stage2/weekthree/Comp/4) Notifications/SmsNotification.cs
@@ -1,6 +1,20 @@
+using Comp._2__Data_Contracts;
+
 namespace Comp._4__Notifications;
 
-public class SmsNotification
+public class SmsNotification :  NotificationBase
 {
-    
+    public SmsDto Sms { get; set; }
+
+    public SmsNotification(SmsDto sms)
+    {
+        Sms = sms;
+    }
+
+    public void SendSms()
+    {
+        Console.WriteLine($"To: {Sms.To}");
+        Console.WriteLine($"From: {Sms.From}");
+        Console.WriteLine($"Message: {Sms.Message}");
+    }
 }
