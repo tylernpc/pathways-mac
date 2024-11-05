a1e05d451446e11e7e1893a913ce0b8c95dfdb74
TYLER LE PC <tle@dontpaniclabs.com>
2024-11-05 12:07:14 -0600

    New Car Collection Program, fix js to adjust for my logic

---
+++ stage2/weekfour/CarCollectionFullStack/Controllers/CarItemsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarCollectionFullStack.Models;
+
+namespace CarCollectionFullStack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarItemsController : ControllerBase
+    {
+        private readonly CarCollectionContext _context;
+
+        public CarItemsController(CarCollectionContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CarItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CarItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        // GET: api/CarItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CarItem>> GetCarItem(long id)
+        {
+            var carItem = await _context.TodoItems.FindAsync(id);
+
+            if (carItem == null)
+            {
+                return NotFound();
+            }
+
+            return carItem;
+        }
+
+        // PUT: api/CarItems/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCarItem(long id, CarItem carItem)
+        {
+            if (id != carItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(carItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CarItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/CarItems
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<CarItem>> PostCarItem(CarItem carItem)
+        {
+            _context.TodoItems.Add(carItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCarItem", new { id = carItem.Id }, carItem);
+        }
+
+        // DELETE: api/CarItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCarItem(long id)
+        {
+            var carItem = await _context.TodoItems.FindAsync(id);
+            if (carItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(carItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CarItemExists(long id)
+        {
+            return _context.TodoItems.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ stage2/weekfour/CarCollectionFullStack/Program.cs
@@ -0,0 +1,40 @@
+using CarCollectionFullStack.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarCollectionFullStack
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddDbContext<CarCollectionContext>(opt => opt.UseInMemoryDatabase("CarCollection"));
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
