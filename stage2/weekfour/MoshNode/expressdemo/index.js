efe1a7f134156ec845c65f2231fdf0ed0a7426c0
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-11-07 14:37:49 -0600

    Fixing Stuff, we're done

--- stage2/weekfour/MoshNode/expressdemo/index.js
+++ stage2/weekfour/MoshNode/expressdemo/index.js
@@ -1,45 +1,90 @@
-const Joi = require('joi');
+const Joi = require("joi");
 const express = require("express");
 const app = express();
 
 app.use(express.json());
 
 const courses = [
-  {id: 1, name: "course1"},
-  {id: 2, name: "course2"},
-  {id: 3, name: "course3"},
-  {id: 4, name: "course4"},
-  {id: 5, name: "course5"}
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+  { id: 4, name: "course4" },
+  { id: 5, name: "course5" },
 ];
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Just making sure it works!");
 });
 
 app.get("/api/courses", (req, res) => {
-    if (!req.body.name || req.body.name.length < 3) {
-        // 400 Bad Request
-        res.status(400).send("Name is required and at a minimum be 3 characters");
-        return;
-    }
+  if (!req.body.name || req.body.name.length < 3) {
+    // 400 Bad Request
+    res.status(400).send("Name is required and at a minimum be 3 characters");
+    return;
+  }
 
   res.send(courses);
 });
 
 app.get("/api/courses/:id", (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id))
-  if (!course) res.status(404).send("The course with the given ID was not found");
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course with the given ID was not found");
   res.send(course);
 });
 
-app.post('/api/courses', (req, res) => {
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    }
-    courses.push(course);
-    res.send(course);
-})
+app.post("/api/courses", (req, res) => {
+  const { error } = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+app.put("/api/courses/:id", (req, res) => {
+  // Look up the course\
+  // If not exist, return 404
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course with the given ID was not found");
+
+  // Validate
+  // If invalid, return 400 - Bad request
+  const { error } = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course with the given ID was not found");
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  return schema.validate(course);
+}
 
 // enviornment variable called port
 // PORT | Value set outside of the application
