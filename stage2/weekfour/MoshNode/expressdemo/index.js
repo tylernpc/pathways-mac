f10e32b3fa7f95088b2e1308157fd93ce1db8418
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-11-07 13:46:29 -0600

    Updates

---
+++ stage2/weekfour/MoshNode/expressdemo/index.js
@@ -0,0 +1,47 @@
+const Joi = require('joi');
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const courses = [
+  {id: 1, name: "course1"},
+  {id: 2, name: "course2"},
+  {id: 3, name: "course3"},
+  {id: 4, name: "course4"},
+  {id: 5, name: "course5"}
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/courses", (req, res) => {
+    if (!req.body.name || req.body.name.length < 3) {
+        // 400 Bad Request
+        res.status(400).send("Name is required and at a minimum be 3 characters");
+        return;
+    }
+
+  res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id))
+  if (!course) res.status(404).send("The course with the given ID was not found");
+  res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course);
+})
+
+// enviornment variable called port
+// PORT | Value set outside of the application
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
