2b4a0d2f92fe632268d08f93e2a61f98e606da07
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-11-12 11:35:26 -0600

    Updates

--- stage2/weekfive/comp/scripts/express.js
+++ stage2/weekfive/comp/scripts/express.js
@@ -1,3 +1,4 @@
+// JOBS CODE BELOW
 const Joi = require("joi");
 const express = require("express");
 const cors = require("cors");
@@ -111,6 +112,111 @@ function validateJobPosting(job) {
   });
 
   return schema.validate(job);
+}
+
+// USERS CODE BELOW
+
+const users = [
+  {
+    userID: 1,
+    userType: "employer",
+    username: "Tyler",
+    password: "password",
+  },
+  {
+    userID: 2,
+    userType: "user",
+    username: "Kodad",
+    password: "luna",
+  }
+];
+
+// create operation
+app.post("/api/users", (req, res) => {
+  const { error } = validateUserCreation(req.body);
+
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  const user = {
+    userID: jobs.length + 1,
+    userType: req.body.userType,
+    username: req.body.username,
+    password: req.body.password
+  };
+
+  users.push(user);
+  res.send(user);
+});
+
+// read operation
+app.get("/api/users/:user", (req, res) => {
+  const user = users.find((u) => u.userID === parseInt(req.params.user));
+  if (!user) {
+    res.status(404).send("Apologies, there was no user matching that");
+  } else {
+    res.send(user);
+  }
+});
+
+app.get("/api/users", (req, res) => {
+  res.send(users);
+});
+
+// update operation || ********* POTENTIALLY NOT DOING AN UPDATE FOR USERS FOR NOW
+// app.put("/api/users/:user", (req, res) => {
+//     const user = users.find((u) => u.userID === parseInt(req.params.user));
+//   if (!user) {
+//     res.status(404).send("Apologies, there was no user matching that.");
+//   } else {
+//     const { error } = validateUserCreation(req.body);
+//     if (error) {
+//       return res.status(400).send(error.details[0].message);
+//     }
+
+//     user.
+//     job.companyName = req.body.companyName;
+//     job.jobTitle = req.body.jobTitle;
+//     job.companyEmail = req.body.companyEmail;
+//     job.jobState = req.body.jobState;
+//     job.description = req.body.description;
+
+//     console.log("Request body: ", req.body);
+//     res.send(job);
+//   }
+// });
+
+// delete operation || ********* POTENTIALLY NOT DOING AN UPDATE FOR USERS FOR NOW
+// app.delete("/api/jobs/:job", (req, res) => {
+//   const job = jobs.find((j) => j.jobID === parseInt(req.params.job));
+//   if (!job) {
+//     res.status(404).send("Apologies, there was no job matching that.");
+//   } else {
+//     const index = jobs.indexOf(job);
+//     jobs.splice(index, 1);
+//     res.send(jobs);
+//   }
+// });
+
+// basic validation function
+function validateUserCreation(user) {
+  const schema = Joi.object({
+    userType: Joi.string().valid("employer", "user").required().messages({
+      "any.only": 'userType must be either "employer" or "user".',
+    }),
+    userName: Joi.string().min(3).max(30).required().messages({
+      "string.empty": "userName is required.",
+      "string.min": "userName must be at least 3 characters long.",
+      "string.max": "userName must be at most 30 characters long.",
+    }),
+    password: Joi.string().min(8).required().messages({
+      "string.empty": "password is required.",
+      "string.min": "password must be at least 8 characters long.",
+    }),
+  });
+
+  return schema.validate(user);
 }
 
 // connection
