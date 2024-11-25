b25fb0a56c61bd7a78ff54d8cd41e848e3aba1ad
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-11-25 09:28:47 -0600

    Updea

--- stage3/postfix/Program.cs
+++ stage3/postfix/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace postfix;
+
+class Program
+{
+    static void Main(string[] strings)
+    {
+        Console.WriteLine("Hello");
+    }
+}
