9ec3e57321b61e848b34737730badd4dc7561c90
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-11-26 12:19:50 -0600

    Updates

--- stage3/PostFixParser/Program.cs
+++ stage3/PostFixParser/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace PostFixParser;
+
+class Program
+{
+    static void Main(string[] strings)
+    {
+
+    }
+
+    enum Operator
+    {
+        Add,
+        Subtract,
+        Multiply,
+        Divide
+    }
+
+    struct Node
+    {
+
+        public Node(string input)
+        {
+            if (input == "+")
+            {
+                Operator = Operator.Add;
+            }
+            else if (input == "-")
+            {
+                Operator = Operator.Subtract;
+            }
+            else if (input == "*")
+            {
+                Operator = Operator.Multiply;
+            }
+            else if (input == "/")
+            {
+                Operator = Operator.Divide;
+            }
+            else
+            {
+                IsNumber = true;
+                Number = int.Parse(input);
+            }
+        }
+
+        public bool IsNumber { get; private set; }
+        public int Number { get; private set; }
+        public Operator Operator { get; set; }
+
+    }
+
+
+
+    class ParserService
+    {
+        public List<Node> Parse(string input)
+        {
+            var nodes = new List<Node>();
+            var split = input.Split(' ');
+            foreach (var item in split)
+            {
+                nodes.Add(new Node(item));
+            }
+            return nodes;
+        }
+    }
+}
